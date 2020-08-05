30790b6cc3b2aec679e0f2a69bb9bab2f374ba73
mert <uzeyirmertciflikli@gmail.com>
2020-08-05 12:52:43 +0200

    create controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,38 @@
+// Get all bootcamps
+// GET /api/v1/bootcamps
+// Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+// Get single bootcamp
+// GET /api/v1/bootcamps/:id
+// Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+};
+
+// Create a bootcamp
+// POST /api/v1/bootcamps
+// Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+};
+
+// Update a bootcamp
+// PUT /api/v1/bootcamps/:id
+// Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// Delete a bootcamp
+// DELETE /api/v1/bootcamps/:id
+// Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
